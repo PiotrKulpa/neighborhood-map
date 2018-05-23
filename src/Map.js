015b4484e1dc70b3470918cb37f6a8a27c25f8ad
PiotrKulpa <p.kulpa@onet.eu>
2018-05-23 21:28:07 +0200

    feat: Add Search & Map components

---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="">
+        Maap component
+      </div>
+    );
+  }
+}
+
+export default Map;
