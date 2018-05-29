5f60a613fec233fb31eeb563379f2725e22a2a28
PiotrKulpa <p.kulpa@onet.eu>
2018-05-29 22:04:38 +0200

    fix: close infowindow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 
   state = {
     places: [
-      {name: 'Castle in Lublin', lat: 51.247063, lng: 22.5669419},
+      {name: 'Castle in Lublin', lat: 51.2504931, lng: 22.5702397},
       {name: 'Old Town', lat: 51.24875, lng: 22.568635},
       {name: 'Fontain', lat: 51.2481725, lng: 22.5595155},
       {name: 'State Park', lat: 51.2485435, lng: 22.5481983},
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -60,9 +60,6 @@ class MapContainer extends Component{
                 infowindow.marker = marker;
                 infowindow.setContent('<div>' + marker.title + '</div>');
                 infowindow.open(map, marker);
-                infowindow.addListener('closeclick', function() {
-                  window.infowindow.setMarker();
-                })
               }
             }
 
