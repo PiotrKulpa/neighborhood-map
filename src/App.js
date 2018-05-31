1c37e25e3cd7df1926522491a83e162ee915df0c
PiotrKulpa <p.kulpa@onet.eu>
2018-05-31 13:34:48 +0200

    fix: search fix

--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ import MapContainer from './MapContainer';
 
 class App extends Component {
 
+  basicPlaces = [
+    {name: 'Castle in Lublin', lat: 51.2504931, lng: 22.5702397},
+    {name: 'Old Town', lat: 51.24875, lng: 22.568635},
+    {name: 'Fontain', lat: 51.2481725, lng: 22.5595155},
+    {name: 'State Park', lat: 51.2485435, lng: 22.5481983},
+    {name: 'Centre for the Meeting of Cultures', lat: 51.2470124, lng: 22.5489064}
+  ]
+
   state = {
-    places: [
-      {name: 'Castle in Lublin', lat: 51.2504931, lng: 22.5702397},
-      {name: 'Old Town', lat: 51.24875, lng: 22.568635},
-      {name: 'Fontain', lat: 51.2481725, lng: 22.5595155},
-      {name: 'State Park', lat: 51.2485435, lng: 22.5481983},
-      {name: 'Centre for the Meeting of Cultures', lat: 51.2470124, lng: 22.5489064}
-    ]
+    places: this.basicPlaces
   }
 
   filterPlaces=(filterKey) => {
+    if(filterKey.length > 0) {
+      //let newPlaces = this.state.places.filter((el) => el.name.toLowerCase().includes(filterKey.toLowerCase()));
+      this.setState((state) => {
+        return {places: this.basicPlaces.filter((el) => el.name.toLowerCase().includes(filterKey.toLowerCase()))}
+    }  )
 
-    let newPlaces = this.state.places.filter((el) => el.name.startsWith(filterKey));
-    // this.setState({
-    //   places: newPlaces
-    // })
-    console.log(newPlaces);
+
+    } else {
+      this.setState({
+        places: this.basicPlaces
+      })
+    }
+
+
 
   }
 
